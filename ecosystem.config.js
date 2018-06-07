b12adb04829a2ada98165ebe0609907eeb423069
dc <1601224968@qq.com>
2018-06-07 18:47:28 +0800

    修改

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  apps : [{
-    name   : "app1",
-    script : "./app.js"
-  }]
-}
+    apps: [
+        {
+            name: 'app',
+            script: './bin/server.js',
+            args: "--env=production",
+            env_production: {
+                NODE_ENV: 'production'
+            }
+        }
+
+    ]
+};
